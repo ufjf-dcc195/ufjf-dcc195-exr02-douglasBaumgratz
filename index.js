c5d3289578e5421cfed58555273d1d3ffc7cff82
douglasBaumgratz <douglas.baumgratz@gmail.com>
2019-08-27 16:13:17 -0300

    Inserindo router oculto.html

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ var requestHandlers = require("./requestHandlers");
 
 var handlers = {};
 handlers["/"] = requestHandlers.senha;
+handlers["/oculto.html"] = requestHandlers.senha;
 
 server.start(router.route, handlers);
